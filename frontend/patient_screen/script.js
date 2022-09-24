8ecba5fdd271733bd6b65447453f38c424a56de6
dui <32657584+phamleduy04@users.noreply.github.com>
2022-09-24 11:24:08 -0500

    push

--- frontend/patient_screen/script.js
+++ frontend/patient_screen/script.js
@@ -1,13 +1,13 @@
 $(document).ready(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const roomID = urlParams.get('roomID');
-    $.get(`http://localhost:3306/data?roomID=${roomID}`, (data) => {
+    $.get(`http://localhost:3000/data?roomID=${roomID}`, (data) => {
         if (!data || data.length === 0) return;
         const firstDoc = data[0];
-        $('body > p').append(`Doctor name: ${firstDoc.name}`);
-        $('body > p1').append(`Doctor ID: ${firstDoc.id}`);
+        $('body > #docdata > p').append(`Doctor name: ${firstDoc.name}`);
+        $('body > #docdata> p1').append(`Doctor ID: ${firstDoc.id}`);
         $('#doctor-image').prepend(`<img src="${firstDoc.pictureURL}" alt="Doctor image" />`);
     });
     
-    setInterval(() => location.reload(), 5000);
+    // setInterval(() => location.reload(), 5000);
 })
