49f7fd1b89c83e85c49aab959c9fa164589b83b0
dui <32657584+phamleduy04@users.noreply.github.com>
2022-09-24 23:16:35 -0500

    Update patient.js

--- public/patient_screen/patient.js
+++ public/patient_screen/patient.js
@@ -13,9 +13,9 @@ function loadDoctor(roomID, array) {
         if (data[0].id == array[0]?.id) return;
         const firstDoc = data[0];
         $('#docPic > div > div.col-md-8 > div > h5').text(`Name: ${firstDoc.name}`);
-        $('#docPic > div > div.col-md-8 > div > p:nth-child(2)').append(`ID: ${firstDoc.id}`);
-        $('#docPic > div > div.col-md-8 > div > p:nth-child(3)').append(`Department : ${firstDoc.department}`);
-        $('#docPic > div > div.col-md-8 > div > p:nth-child(4)').append(`Postion : ${firstDoc.position}`);
+        $('#docPic > div > div.col-md-8 > div > p:nth-child(2)').text(`ID: ${firstDoc.id}`);
+        $('#docPic > div > div.col-md-8 > div > p:nth-child(3)').text(`Department : ${firstDoc.department}`);
+        $('#docPic > div > div.col-md-8 > div > p:nth-child(4)').text(`Postion : ${firstDoc.position}`);
         $('#doctor-image').prepend(`<img src="/image?fileName=${firstDoc.pictureURL}" alt="Doctor image" />`);
         array.push(firstDoc);
         
